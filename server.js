4e110b0c79340ee9f71c8397547a3f8249bf02d8
Derek Arrotta <derek.arrotta@gmail.com>
2021-01-09 17:10:47 -0500

    setup security and make sure frnt end works

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
 const POKEDEX = require('./pokedex.json')
-
-console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
-
-const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+app.use(helmet())
+app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
@@ -20,21 +20,37 @@ app.use(function validateBearerToken(req, res, next) {
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
   }
-  
   // move to the next middleware
   next()
 })
 
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
 function handleGetTypes(req, res) {
   res.json(validTypes)
 }
-  
 app.get('/types', handleGetTypes)
 
 function handleGetPokemon(req, res) {
-  res.send('Hello, Pokemon!')
+  let response = POKEDEX.pokemon;
+
+  // filter our pokemon by name if name query param is present
+  if (req.query.name) {
+    response = response.filter(pokemon =>
+      // case insensitive searching
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    )
+  }
+
+  // filter our pokemon by type if type query param is present
+  if (req.query.type) {
+    response = response.filter(pokemon =>
+      pokemon.type.includes(req.query.type)
+    )
+  }
+
+  res.json(response)
 }
-  
 app.get('/pokemon', handleGetPokemon)
 
 const PORT = 8000
